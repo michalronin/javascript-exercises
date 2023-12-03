72513d113ac45de4edcb717364d2902db7b7f9b8
Michal Iwanski <michalronin@outlook.com>
2023-12-03 17:34:57 +0100

    Exercise 06 done, all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+  let result = [];
+  if (first < 0 || last < 0) {
+    return 'ERROR';
+  }
 
+  if ((typeof first) !== "number" || (typeof last) !== "number") {
+    return 'ERROR';
+  }
+
+  if (first > last) {
+    for (let i = last; i <= first; i++) {
+      result.push(i);
+    }
+  } else { 
+    for (let i = first; i <= last; i++) {
+      result.push(i);
+    }
+  }
+  return result.reduce((partialSum, a) => partialSum + a, 0);
 };
 
 // Do not edit below this line
